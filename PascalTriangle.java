7562de89f7a068a39deb373441d6cbc2a6c31761
Arpitananda123 <arpitananda.11th.a@gmail.com>
2025-05-20 08:28:20 +0530

    Pascal Triangle

---
+++ PascalTriangle.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example test case
+        int rowIndex = 4;
+        List<Integer> row = solution.getRow(rowIndex);
+
+        System.out.println("Pascal's Triangle Row " + rowIndex + ": " + row);
+    }
+}
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for (int i = 0; i < rowIndex; i++) {
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            for (int j = 1; j < row.size(); j++) {
+                newRow.add(row.get(j - 1) + row.get(j));
+            }
+            newRow.add(1);
+            row = newRow;
+        }
+
+        return row;
+    }
+}
